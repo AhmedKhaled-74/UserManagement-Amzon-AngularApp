1a018fceac5b4381f9321f522f5e20fd2df91463
AhmedKhaled-74 <ahmed.khaled.lalo@gmail.com>
2025-09-11 04:33:50 +0300

    initial commit to all user management ui files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { Notfound } from './notfound/notfound.js';
 
-export const routes: Routes = [];
+import { Register } from './register/register.js';
+import { Login } from './login/login.js';
+import { authGuard } from './guards/auth-guard.js';
+import { ConfirmEmail } from './confirm-email/confirm-email.js';
+import { ConfirmEmailSuccess } from './confirm-email-success/confirm-email-success.js';
+import { Home } from './home/home.js';
+import { ResetPassword } from './reset-password/reset-password.js';
+import { Profile } from './profile/profile.js';
+import { Users } from './users/users.js';
+import { AdminGuard } from './guards/admin-guard.js';
+import { Roles } from './roles/roles.js';
+import { Permissions } from './permissions/permissions.js';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'account/login', pathMatch: 'full' },
+
+  { path: 'account/register', component: Register },
+  { path: 'account/login', component: Login },
+  { path: 'account/confirm-email', component: ConfirmEmail },
+  { path: 'account/reset-password', component: ResetPassword },
+  {
+    path: 'account/confirm-email-success',
+    component: ConfirmEmailSuccess,
+    canActivate: [authGuard],
+  },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  {
+    path: 'admin/users',
+    component: Users,
+    canActivate: [authGuard, AdminGuard],
+  },
+  {
+    path: 'admin/roles',
+    component: Roles,
+    canActivate: [authGuard, AdminGuard],
+  },
+  {
+    path: 'admin/permissions',
+    component: Permissions,
+    canActivate: [authGuard, AdminGuard],
+  },
+
+  { path: '**', component: Notfound },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,73 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AccountService } from './Services/account-service.js';
+import { isPlatformBrowser, NgIf, AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    FontAwesomeModule,
+    NgIf,
+    AsyncPipe,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('LoginApp');
+export class App implements OnInit {
+  protected readonly title = signal('OrderManagement');
+  constructor(
+    public Acc: AccountService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+  ngOnInit() {
+    this.loadUser();
+  }
+  private loadUser(): void {
+    // Only run this in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      const token =
+        localStorage.getItem('token') || sessionStorage.getItem('token');
+      const userName =
+        localStorage.getItem('userName') || sessionStorage.getItem('userName');
+      const userRole =
+        localStorage.getItem('userRole') || sessionStorage.getItem('userRole');
+
+      if (token && userName) {
+        this.Acc.setUserName(userName);
+        this.Acc.setUserRole(userRole);
+      }
+    }
+  }
+
+  Logout() {
+    this.Acc.LogOut().subscribe({
+      next: () => {
+        // Handle successful logout (e.g., redirect)
+        this.Acc.setUserName(null);
+        this.Acc.setUserRole(null);
+        localStorage.removeItem('token');
+        sessionStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        sessionStorage.removeItem('refreshToken');
+        localStorage.removeItem('userName');
+        sessionStorage.removeItem('userName');
+        localStorage.removeItem('userRole');
+        sessionStorage.removeItem('userRole');
+        this.router.navigate(['/account/login']);
+      },
+      error: (err) => {
+        console.error('Logout failed:', err);
+      },
+    });
+  }
 }
